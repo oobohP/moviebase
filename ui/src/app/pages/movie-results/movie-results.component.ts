d658991a31e4beba1f07bb391a8578137561aadb
oobohP <stevenly1616@gmail.com>
2021-01-28 22:39:09 -0800

    Added movie results component and pages module

---
+++ ui/src/app/pages/movie-results/movie-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-results',
+  templateUrl: './movie-results.component.html',
+  styleUrls: ['./movie-results.component.scss']
+})
+export class MovieResultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ ui/src/app/pages/pages.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+
+// Pages
+
+@NgModule({
+  declarations: [
+    // Pages go here
+  ],
+  imports: [
+    SharedModule
+  ],
+  providers: [
+
+  ],
+})
+export class PagesModule { }
